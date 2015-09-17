e6575fe6eddb34811241190f7eee031bc5b3654c
ANDREW CANIDA <MR.ANDREW.CANIDA@GMAIL.COM>
2015-09-17 11:54:42 -0700

    saving lucky button and alert message

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,13 +1,58 @@
-$(document).ready(function() {
-  $("button").on('click', function() {
-    var buttonID = $(this).attr('id');
+$(function () {
 
-    $.get("http://localhost:3000/"+buttonID, function(response) {
+  $('.get-info').click(function () {
+    var section = $(this).attr('id');
 
-      $('#show_'+buttonID).append(response+"<br>");
-      });
+    $.get("http://localhost:3000/" + section, function (response) {
+      $("#show-" + section).text(response[0].topic);
+    });
   });
+
+  // Code Challenge 1:
+  // When the user clicks the "Find It" button,
+  // let's get back all the Lectures notes and display them.
+  $('#search').on('submit', function(e) {
+    e.preventDefault();
+
+    var searchParam = $('input[name=searchText]').val();
+    var lucky = $('input[name=lucky]').is(':checked');
+    var data = {searchText: searchParam, lucky: lucky};
+    var url = 'http://localhost:3000/search';
+
+    console.log(data); // this will be a sanity check to see that we're getting the data
+    $.post(url, data, function(response) {
+      console.log(response);
+      var lectures,
+          output = '';
+
+      // lectures = response.lectures;
+
+      // for (var i = 0; i < lectures.length; i++) {
+      //   if (lectures[i].notes && lectures[i].notes.constructor === Array) {
+      //     for (var j = 0; j < lectures[i].notes.length; j++) {
+      //       output += '<li>' + lectures[i].notes[j] + '</li>';
+      //     }
+      //   }
+      // }
+      if ( response.notes >= 0 ) {
+        alert( "Sorry there were no matches found" );
+      }
+
+      if (response.notes && response.notes.constructor === Array) {
+        for (var i = 0; i < response.notes.length; i++) {
+          output += '<li>' + response.notes[i] + '</li>';
+        }
+      }
+
+      output = '<ul>' + output + '</ul>';
+
+      $("#search-results").html(output);
+    });
+  });
+
 });
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,68 +1,36 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/app"));
+var bodyparser = require("body-parser");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname + '/app/'));
 
-var course = {
-  level: 201,
-  name: "F2 Javascript",
-  lectures: [
-  { topic:"Lecture 1: Javascipt Basics"},
-  { topic:"Lecture 2: Intro to Node"},
-  { topic:"Lecture 3: Array Methods, Functions & Scope"},
-  { topic:"Lecture 4: OOP in JS & Classes/Inheritance"},
-  { topic:"Lecture 5: JS in the browser & Intro to jQuery"},
-  { topic:"Lecture 6: Building a server & Using Ajax"},
-  { topic:"Lecture 7: Build & Deploy an App"},
-  { topic:"Lecture 8: Functional Programming with Iodash"}],
+var f2search = require("./lib/f2search");
 
+app.post('/search', function (req, res) {
+  var searchResults = f2search( req.body.searchText, req.body.lucky );
+  res.json(searchResults);
+});
 
-  labs: [
-    { topic:"Lab 1: Assertions About Zoo Animals"},
-    { topic:"Lab 1: Translating Meerkats"},
-    { topic:"Lab 1: Random Favorite Animal"},
-    { topic:"Lab 1: Hungry Lion"},
-    { topic:"Lab 2: Blob Consumption Rate"},
-    { topic:"Lab 2: Sorting Arrays"},
-    { topic:"Lab 2: Universal Translator"}]
+app.get('/lectures', function (req, res) {
+  res.json(course.lectures);
+});
 
-  };
+app.get('/labs', function (req, res) {
+  res.json(course.labs);
+});
 
-app.listen(port, function() {
+app.get('/course_name', function (req, res) {
+  res.json(course.name);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile();
+});
+
+app.listen(port, function () {
   console.log('server started on port ' + port);
 });
-
-app.get("/lectures", function(req, res) {
-  var randomIndex = Math.floor(Math.random() * course.lectures.length);
-  res.json(course.lectures[randomIndex].topic);
-
-});
-
-app.get("/labs", function(req, res) {
-  var randomIndex = Math.floor(Math.random() * course.labs.length);
-  res.json(course.labs[randomIndex].topic);
-});
-
-
-
-
-
-
-
-var jokes = [
- { setup: "What's the difference between a guitar and a fish?",
- punchline: "You can't tuna fish." },
- { setup: "What do you get when you cross a cow and a duck?",
- punchline: "Milk and quackers." },
- { setup: "How many tickles does it take to make an octupus laugh?",
- punchline: "Ten Tickles" }
-];
-
-app.get("/joke", function(req, res) {
- var randomIndex = Math.floor(Math.random() * jokes.length);
- res.json(jokes[randomIndex]);
-});
-
-
